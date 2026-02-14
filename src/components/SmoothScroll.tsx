3b7b9e691c6019601eb6d964e3db0a3ec7963e36
Saveen Salah <saveensalahpec18cs022@gmail.com@example.com>
2026-02-14 10:21:10 +0530

    Fix Lenis smooth scroll and Vercel build error

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,26 +5,29 @@ import Lenis from 'lenis'
 
 export default function SmoothScroll({ children }: { children: ReactNode }) {
   useEffect(() => {
-        const lenis = new Lenis({
-            duration: 1.2,
-            easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-            gestureDirection: 'vertical',
-            smooth: true,
-            smoothTouch: true,
-            touchMultiplier: 2,
-        }  )
 
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t: number) =>
+        Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      touchMultiplier: 2,
+    })
+
+    let animationFrameId: number
 
     function raf(time: number) {
       lenis.raf(time)
-      requestAnimationFrame(raf)
+      animationFrameId = requestAnimationFrame(raf)
     }
 
-    requestAnimationFrame(raf)
+    animationFrameId = requestAnimationFrame(raf)
 
     return () => {
       lenis.destroy()
+      cancelAnimationFrame(animationFrameId)
     }
+
   }, [])
 
   return <>{children}</>
